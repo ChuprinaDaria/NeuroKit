82086d1edcddeb06fddf20a4df111661928e6f2d
Daria Chuprina <chuprina.dariia@gmail.com>
2025-07-02 08:25:57 +0200

    Fm states

--- handlers/constants.py
+++ handlers/constants.py
@@ -1,11 +1,17 @@
 from enum import IntEnum
+from aiogram.fsm.state import StatesGroup, State
 
 class States(IntEnum):
     START = 0
     PHONE = 1
     TEST_MENU = 2
     TEST_IN_PROGRESS = 3
-    ADMIN_MESSAGE = 4
     WAITING_FOR_USERNAME = 5
 
 AI_CONSULT = 10
+
+class FSMStates(StatesGroup):
+    START = State()
+    PHONE = State()
+    TEST_MENU = State()
+    ADMIN_MESSAGE = State()
